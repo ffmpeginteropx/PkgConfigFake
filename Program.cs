6f0208fce680e9291afb30ef9a3ac52bf150f1e4
Lukas Fellechner <lukas.fellechner@gmx.net>
2020-01-14 21:59:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkgConfigFake
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int result = 1;
+            if (args.Length == 1 && args[0] == "--version")
+            {
+                Console.WriteLine("1.0");
+                result = 0;
+            }
+            else if (args.Length > 1 && args[0] == "--exists")
+            {
+                var lib = args.Last();
+                if (lib == "libxml-2.0")
+                {
+                    Console.WriteLine($"{lib} is installed.");
+                    result = 0;
+                }
+            }
+            else if (args.Length > 1 && args[0] == "--libs")
+            {
+                var lib = args.Last();
+                if (lib == "libxml-2.0")
+                {
+                    Console.WriteLine("-llibxml2 -lz");
+                    result = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
